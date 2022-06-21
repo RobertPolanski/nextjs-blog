0dcf3593454508d8e2a6432246f2ff99ed04be32
Robert Polanski <robert.polanski@instersoft.de>
2022-06-21 16:08:17 +0200

    init

---
+++ pages/posts/first-post.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout, {siteTitle} from "../../components/layout";
+import utilStyles from "../../styles/utils.module.css";
+
+
+export default function FirstPost() {
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <section className={utilStyles.headingMd}>
+                <p>I'm Ropcio</p>
+                <p>
+                    (This is a sample website - you’ll be building a site like this on{' '}
+                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+                </p>
+                <p>
+                    <Link href="/">
+                        <a>Back to home</a>
+                    </Link>
+                </p>
+            </section>
+        </Layout>
+    );
+}
